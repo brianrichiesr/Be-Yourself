eb2309a393b594d6af1a0a239e397688cf2fd2ce
Brian Richie <brianrichiesr@Brians-MacBook-Pro.local>
2023-12-08 11:32:42 -0800

    setup initial routes, formik, yup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+// import React, { useEffect, useState } from "react";
+// import { Switch, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <div>
+      <Navbar />
+      <h1>App Header</h1>
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,65 @@
-import React from "react";
-import App from "./components/App";
-import "./index.css";
-import { createRoot } from "react-dom/client";
+// import React from "react";
+// import App from "./components/App";
+// import "./index.css";
+// import { createRoot } from "react-dom/client";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(<App />);
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import "./index.css"
+import App from "./components/App";
+import Post from "./components/Post";
+import Error from "./components/Error";
+import Landing from "./components/Landing";
+import Signup from "./components/Signup";
+import Main from "./components/Main";
+import Submission from "./components/Submission";
+import Login from "./components/Login";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Landing />
+      }, 
+      {
+        path: "/login",
+        element: <Login />
+      }, 
+      {
+        path: "/signup",
+        element: <Signup />
+      }, 
+      {
+        path: "/main",
+        element: <Main />
+      }, 
+      {
+        path: "/submission",
+        element: <Submission />
+      }, 
+      {
+        path: "/posts",
+        element: <Post />
+      }
+    ]
+  }
+])
+
+root.render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
