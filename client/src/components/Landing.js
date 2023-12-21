874cf1064c54a4cf8b3ab588c9b65757abe7b2e4
Brian Richie Sr <brianrichiesr@gmail.com>
2023-12-21 10:14:35 -0800

    add delete posts by author and landing css

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -11,7 +11,7 @@ function Landing() {
         <div id="motto"><span>Love</span> <span>Support</span> <span>Celebrate</span></div>
         <section className="imgBlock">
           <div className="imgContainer img-1"></div>
-          <div className="imgText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+          <div className="imgText">"The best and most beautiful things in this world cannot be seen or even heard, but must be felt with the heart." — Helen Keller</div>
         </section>
         <section className="supportText">
           <div>Give The <span>Love</span> You Can</div>
@@ -20,11 +20,11 @@ function Landing() {
           <section className="imgBlock-2">
             <div className="imgBlock-2-container">
             <div className="imgContainer imgContainer-2 img-2"></div>
-            <div className="imgText imgText-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+            <div className="imgText imgText-2">"Where there is love there is life."<p> — Mahatma Gandhi</p></div>
             </div>
             <div className="imgBlock-2-container columnReverse">
             <div className="imgContainer imgContainer-2 img-3"></div>
-            <div className="imgText imgText-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+            <div className="imgText imgText-2">"I can't promise to fix all your problems, but I can promise you will never have to face them alone." — Anonymous</div>
             </div>
           </section>
           <section className="supportText">
@@ -32,7 +32,7 @@ function Landing() {
           </section>
           <section className="imgBlock-3">
             <div className="imgContainer img-4"></div>
-            <div className="imgText fullWidth">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+            <div className="imgText fullWidth">"The greatest happiness of life is the conviction that we are loved; loved for ourselves, or rather, loved in spite of ourselves." — Victor Hugo</div>
           </section>
       </main>
     </div>
